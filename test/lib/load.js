a3d7ee27430cf0db23b6c34575a40094df26d837
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2015-01-15 12:56:36 -0500

    Flesh out load tests

--- test/lib/load.js
+++ test/lib/load.js
@@ -2,25 +2,85 @@ var should = require('should');
 var load = require('../../lib/load');
 
 describe('load', function(){
+  beforeEach(function(){
+    taskList = {}
+  })
+
   describe('when passed a JSON object', function(){
     describe('when JSON is valid', function(){
-      it('adds the tasks to the global taskList');
+      var tasks = {
+        "taskname" : {
+          "remote" : "localhost",
+          "cwd" : "/",
+          "commands" : [
+            "echo I'm a task!",
+            "echo I'm another task!",
+            "echo Hello, my name is {{name}}"
+          ]
+        }
+      }
+
+      it('adds the tasks to the global taskList', function(){
+        load(tasks)
+        taskList.should.containEql({
+          "taskname" : {
+            "remote" : "localhost",
+            "cwd" : "/",
+            "commands" : [
+              "echo I'm a task!",
+              "echo I'm another task!",
+              "echo Hello, my name is {{name}}"
+            ]
+          }
+        });
+      });
     });
 
     describe('when JSON is missing remote', function(){
-      it('throws an error');
+      var tasks = {
+        "taskname" : {
+          "cwd" : "/",
+          "commands" : [
+            "echo I'm a task!",
+            "echo I'm another task!",
+            "echo Hello, my name is {{name}}"
+          ]
+        }
+      }
+
+      it('throws an error', function(){
+        load.bind(null, tasks).should.throw();
+      });
     });
 
     describe('when JSON is missing cwd', function(){
-      it('throws an error');
+      var tasks = {
+        "taskname" : {
+          "remote" : "localhost",
+          "commands" : [
+            "echo I'm a task!",
+            "echo I'm another task!",
+            "echo Hello, my name is {{name}}"
+          ]
+        }
+      }
+
+      it('throws an error', function(){
+        load.bind(null, tasks).should.throw();
+      });
     });
 
     describe('when JSON is missing commands', function(){
-      it('throws an error');
-    });
+      var tasks = {
+        "taskname" : {
+          "remote" : "localhost",
+          "cwd" : "/"
+        }
+      }
 
-    describe('when there is a syntax error', function(){
-      it('throws an error');
+      it('throws an error', function(){
+        load.bind(null, tasks).should.throw();
+      });
     });
   });
 
@@ -29,36 +89,51 @@ describe('load', function(){
       describe('when JSON is valid', function(){
         var filepath = '../fixtures/valid.json';
 
-        it('adds the tasks to the global taskList');
+        it('adds the tasks to the global taskList', function(){
+          load(filepath)
+          taskList.should.containEql({
+            "taskname" : {
+              "remote" : "localhost",
+              "cwd" : "/",
+              "commands" : [
+                "echo I'm a task!",
+                "echo I'm another task!",
+                "echo Hello, my name is {{name}}"
+              ]
+            }
+          });
+        });
       });
 
       describe('when JSON is missing remote', function(){
         var filepath = '../fixtures/missing-remote.json';
 
-        it('throws an error');
+        it('throws an error', function(){
+          load.bind(null, filepath).should.throw();
+        });
       });
 
       describe('when JSON is missing cwd', function(){
         var filepath = '../fixtures/missing-cwd.json';
 
-        it('throws an error');
+        it('throws an error', function(){
+          load.bind(null, filepath).should.throw();
+        });
       });
 
       describe('when JSON is missing commands', function(){
         var filepath = '../fixtures/missing-commands.json';
 
-        it('throws an error');
-      });
-
-      describe('when there is a syntax error', function(){
-        var filepath = '../fixtures/syntax-error.json';
-
-        it('throws an error');
+        it('throws an error', function(){
+          load.bind(null, filepath).should.throw();
+        });
       });
     });
 
     describe('when path is invalid', function(){
-      it('throws an error');
+      it('throws an error', function(){
+        load.bind(null, 'wenk').should.throw();
+      });
     });
   });
 });
